c4fbc452b36bccdcf5bd433ae8fa530d4e076602
ahmet <ayildirim3457@protonmail.com>
2022-04-21 04:56:49 +0200

    third

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -2,5 +2,6 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("Merhaba");
         System.out.println("Merhaba2");
+        System.out.println("Merhaba3");
     }
 }
